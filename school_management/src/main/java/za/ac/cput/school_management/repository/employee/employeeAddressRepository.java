e3dfc36d3967d2b3d9b6cb11de3e09c1b75eba47
Thearox <tyb0r@live.co.za>
2022-06-18 18:54:26 +0200

    test commit

--- school_management/src/main/java/za/ac/cput/school_management/repository/employee/employeeAddressRepository.java
+++ school_management/src/main/java/za/ac/cput/school_management/repository/employee/employeeAddressRepository.java
@@ -1,4 +1,7 @@
 package za.ac.cput.school_management.repository.employee;
 
-public interface employeeAddressRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.school_management.domain.employee.Employee;
+
+public interface EmployeeAddressRepository extends JpaRepository <Employee, String> {
 }
