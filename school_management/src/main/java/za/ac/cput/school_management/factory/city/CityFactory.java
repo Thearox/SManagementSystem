a59830b66ec3ac2381f51e4f70ad8e7e2666ff3a
217245919 <lylehaines.lh.lh@gmail.com>
2022-06-18 19:08:35 +0200

    new work

---
+++ school_management/src/main/java/za/ac/cput/school_management/factory/city/CityFactory.java
@@ -0,0 +1,34 @@
+package za.ac.cput.school_management.factory.city;
+
+/*
+Mogammad-Redar Behardien _ 216234107
+ADP3 JUNE ASSIGNMENT
+CityFactory.java
+*/
+
+
+import za.ac.cput.school_management.domain.city.City;
+import za.ac.cput.school_management.domain.country.Country;
+import za.ac.cput.school_management.helper.StringHelper;
+
+public class CityFactory {
+
+    public static City createCity(String id, String name, Country country) {
+        if (StringHelper.isEmptyOrNull(id) || StringHelper.isEmptyOrNull(name)
+                || StringHelper.isEmptyOrNull(country.getName()) || StringHelper.isEmptyOrNull(country.getName())) {
+            if (id == null || id.isEmpty())
+                throw new IllegalArgumentException("City id is required, null or empty found!");
+            if (name == null || name.isEmpty())
+                name = "";
+
+        }
+
+        City city = new City.Builder()
+                .setId(id)
+                .setName(name)
+                .setCountry(country)
+                .build();
+        return city;
+    }
+
+}
